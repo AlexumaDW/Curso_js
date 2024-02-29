f3a51b4d64e361798f9562bf2c8585e090d66dc7
alexuma <alexumadw@gmail.com>
2024-02-29 04:41:13 -0300

    creando elementos y fragmentos

--- js/dom.js
+++ js/dom.js
@@ -51,7 +51,7 @@ console.clear();
 
 /**************************DOM - Estilos y Variables**************************/
 
-const $linkDOMCss = document.styleSheets[1].cssRules[3].style;
+/*const $linkDOMCss = document.styleSheets[1].cssRules[3].style;
 const cssObj = getComputedStyle($linkDOM, null);
 console.log($linkDOM.getComputedStyle);
 console.log($linkDOMCss);
@@ -61,11 +61,11 @@ $linkDOM.style.width = "50%";
 $linkDOM.style.textAlign = "center";
 $linkDOM.style.padding = "1rem";
 $linkDOM.style.marginRight = "auto";
-$linkDOM.style.marginLeft = "auto";
+$linkDOM.style.marginLeft = "auto";*/
 
 //custom property
 
-$linkDOMCss.setProperty("background-color", "rgb(165, 42, 42)");
+/*$linkDOMCss.setProperty("background-color", "rgb(165, 42, 42)");
 $body = document.body;
 const $linkRoot = document.styleSheets[1];
 const $DOM = document.documentElement;
@@ -76,7 +76,7 @@ let varDarkColor = getComputedStyle($DOM).getPropertyValue("--dark-color"),
 console.log(varDarkColor);
 console.log(varYellowColor);
 
-$body.style.backgroundColor = varDarkColor;
+$body.style.backgroundColor = varDarkColor;*/
 console.clear();
 /*************************************Clases css*************************************/
 
@@ -111,11 +111,85 @@ console.clear();
 
 /*******************************Recorriendo el DOM*******************************/
 
-const $cards = document.querySelector(".cards");
+//sconst $cards = document.querySelector(".cards");
+/*s                                               *SIEMPRE* que quieras cargar te pido por favor que *CONSULTES EL CVU A ESTE NUMERO* antes de enviar el dinero ya que los vamos rotando para poder ofrecer una mejor atención! ☺
+*SIEMPRE* que quieras cargar te pido por favor que *CONSULTES EL CVU A ESTE NUMERO* antes de enviar el dinero ya que los vamos rotando para poder ofrecer una mejor atención! ☺
 
 console.log($cards);
 console.log($cards.children);
 console.log($cards.children[0]);
 console.log($cards.parentElement);
 console.log($cards.firstElementChild);
-console.log($cards.lastElementChild);
+console.log($cards.lastElementChild);*/
+console.clear();
+
+/****************************Creando Elementos y Fragmentos***************************/
+
+const $figure = document.createElement("figure"),
+  $img = document.createElement("img"),
+  $figcaption = document.createElement("figcaption"),
+  $figcaptionText = document.createTextNode("Blanco"),
+  $cards = document.querySelector(".cards"),
+  $figure2 = document.createElement("figure");
+
+$img.setAttribute("src", "https://place-hold.it/300");
+$img.setAttribute("alt", "None");
+$figure.classList.add("card");
+$figure2.classList.add("card");
+
+$cards.appendChild($figure);
+$figure.appendChild($img);
+$figure.appendChild($figcaption);
+$figcaption.appendChild($figcaptionText);
+$cards.appendChild($figure2);
+
+$figure2.innerHTML = `        
+<img src="https://place-hold.it/300" alt="None">
+<figcaption>none</figcaption>
+</img>`;
+
+const estaciones = ["verano", "primavera", "otoño", "invierno"];
+$ul = document.createElement("ul");
+document.getElementById("estaciones").appendChild($ul);
+
+estaciones.forEach((el) => {
+  const $li = document.createElement("li");
+  $li.textContent = el;
+  $ul.appendChild($li);
+});
+
+const continentes = ["asia", "europa", "america", "africa", "oceania"];
+$ul2 = document.createElement("ul");
+document.getElementById("continentes").appendChild($ul2);
+continentes.forEach((el) => {
+  $ul2.innerHTML += `<li>${el}</li>`;
+});
+
+const meses = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+];
+$ul3 = document.createElement("ul");
+$fragment = document.createDocumentFragment();
+
+meses.forEach((el) => {
+  const $li = document.createElement("li");
+  $li.textContent = el;
+  $fragment.appendChild($li);
+});
+
+$ul3.appendChild($fragment);
+document.getElementById("meses").appendChild($ul3);
+console.clear();
+
+/****************************Templates***************************/
